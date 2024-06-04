5c6984926314826e7e3de27b2467e941e79648e5
bintangrayhan <127047367+bintangrayhan@users.noreply.github.com>
2024-06-04 23:34:57 +0700

    Update Cots_bintang.java

--- src/main/java/com/mycompany/cots_bintang/Cots_bintang.java
+++ src/main/java/com/mycompany/cots_bintang/Cots_bintang.java
@@ -16,6 +16,24 @@ public class Cots_bintang {
         System.out.println("3. Perkalian (*)");
         System.out.println("4. Pembagian (/)");
 
+        System.out.println("Pilih operasi yang diinginkan:");
+        System.out.println("1. Penambahan (+)");
+        System.out.println("2. Pengurangan (-)");
+        System.out.println("3. Perkalian (*)");
+        System.out.println("4. Pembagian (/)");
+
+        System.out.println("Pilih operasi yang diinginkan:");
+        System.out.println("1. Penambahan (+)");
+        System.out.println("2. Pengurangan (-)");
+        System.out.println("3. Perkalian (*)");
+        System.out.println("4. Pembagian (/)");
+
+        System.out.println("Pilih operasi yang diinginkan:");
+        System.out.println("1. Penambahan (+)");
+        System.out.println("2. Pengurangan (-)");
+        System.out.println("3. Perkalian (*)");
+        System.out.println("4. Pembagian (/)");
+
         System.out.print("Masukkan pilihan (1/2/3/4): ");
         int pilihan = scanner.nextInt();
 
